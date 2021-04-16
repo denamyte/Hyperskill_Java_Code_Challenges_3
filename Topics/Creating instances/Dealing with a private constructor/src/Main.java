4862940425029542492d255d42866650c7ba38d3
denamyte <denamyte@gmail.com>
2021-04-16 16:33:31 +0300

    2021-04-16: 1 problem

--- Topics/Creating instances/Dealing with a private constructor/src/Main.java
+++ Topics/Creating instances/Dealing with a private constructor/src/Main.java
@@ -5,33 +5,46 @@ class InstanceUtils {
 
     public static Person createPerson(String name, int age) {
         Class<Person> personClass = Person.class;
-        final Constructor<?>[] constructors = personClass.getDeclaredConstructors();
-        Arrays.stream(constructors).forEach(c -> {
-            for (Class<?> param : c.getParameterTypes()) {
-                System.out.println(param);
-            }
-        });
-
-        return null;
+        final Constructor<> twoParamConstructor = Arrays.stream(personClass.getDeclaredConstructors())
+                .filter(c -> c.getParameterCount() == 2)
+                .findAny().orElseThrow();
+        try {
+            twoParamConstructor.setAccessible(true);
+            return (Person) twoParamConstructor.newInstance(name, age);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
-    public static void main(String[] args) {
-        createPerson("Andrew", 25);
-    }
+//    public static void main(String[] args) {
+//        final Person andrew = createPerson("Andrew", 25);
+//        if (andrew == null) {
+//            System.out.println("null");
+//        } else {
+//            System.out.printf("Name: %s; age: %d", andrew.getName(), andrew.getAge());
+//        }
+//    }
+
 }
 
-class Person {
-    private String name = "unknown";
-    private int age;
-
-    private Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    private Person(int age) {
-        this.age = age;
-    }
-
-    // getters
-}
+//class Person {
+//    private String name = "unknown";
+//    private int age;
+//
+//    private Person(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    private Person(int age) {
+//        this.age = age;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//}
