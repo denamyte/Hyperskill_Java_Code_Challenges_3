688f302d6ab747963e09145fb03309f916312b87
denamyte <denamyte@gmail.com>
2021-04-17 23:36:56 +0300

    2021-04-17: 2 problems

---
+++ Topics/Type Bounds/Writing a class/src/Main.java
@@ -0,0 +1,11 @@
+class Shelf<T extends Book___> {
+    T element;
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+}
