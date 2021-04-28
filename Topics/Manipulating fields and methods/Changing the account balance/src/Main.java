b14be9ea172b6f17ea66b73687dff498eb2e725c
denamyte <denamyte@gmail.com>
2021-04-28 14:45:20 +0300

    2021-04-28: 2 problems

--- Topics/Manipulating fields and methods/Changing the account balance/src/Main.java
+++ Topics/Manipulating fields and methods/Changing the account balance/src/Main.java
@@ -1,8 +1,16 @@
+import java.lang.reflect.Field;
+
 final class AccountUtils {
 
     private AccountUtils() { }
 
     public static void increaseBalance(Account account, long amount) {
-        // write your code here
+        try {
+            final Field balance = Account.class.getDeclaredField("balance");
+            balance.setAccessible(true);
+            balance.set(account, (Long) balance.get(account) + amount);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Topics/Manipulating fields and methods/Field value/src/Main.java
+++ Topics/Manipulating fields and methods/Field value/src/Main.java
@@ -1,11 +1,26 @@
-import java.lang.reflect.Field;
 /**
  * Get value for a given public field or null if the field does not exist or is not accessible.
  */
 class FieldGetter {
 
-    public Object getFieldValue(Object object, String fieldName) throws IllegalAccessException {
-
+    public Object getFieldValue(Object object, String fieldName) {
+        try {
+            return object.getClass().getField(fieldName).get(object);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            return null;
+        }
     }
 
+//    static class TestClass {
+//        private int privateField = -1;
+//        public int publicField = 1;
+//    }
+//
+//    public static void main(String[] args) {
+//        final TestClass instance = new TestClass();
+//        final FieldGetter fg = new FieldGetter();
+//        Stream.of("nonExistent", "privateField", "publicField")
+//                .forEach(fieldName -> System.out.printf(
+//                        "Getting the field '%s' value: %s%n", fieldName, fg.getFieldValue(instance, fieldName)));
+//    }
 }
