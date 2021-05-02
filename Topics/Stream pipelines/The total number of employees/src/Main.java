f24d1e3385d7427d0bff4a9513e04323bf53fef2
denamyte <denamyte@gmail.com>
2021-05-02 14:54:18 +0300

    2021-05-02: 1 problem

---
+++ Topics/Stream pipelines/The total number of employees/src/Main.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class EmployeesCounter {
+
+    static Map<Integer, Integer> trashMap;
+    static HashMap<Integer, Integer> trashHashMap;
+    static Scanner trashScanner;
+    static Collectors trashCollectors;
+    static Arrays trashArrays;
+    static ArrayList<Integer> trashArrayList;
+
+    /**
+     * Calculates the number of employees with salary >= threshold (only for 111- departments)
+     *
+     * @param departments are list of departments
+     * @param threshold is lower edge of salary
+     *
+     * @return the number of employees
+     */
+    public static long calcNumberOfEmployees(List<Department> departments, long threshold) {
+        return departments == null ? 0
+                : departments.stream().filter(d -> d.getCode().startsWith("111-"))
+                .flatMap(d -> d.getEmployees().stream())
+                .filter(e -> e.getSalary() >= threshold)
+                .count();
+    }
+
+    // Don't change the code below
+    static class Employee {
+        private final String name;
+        private final Long salary;
+
+        Employee(String name, Long salary) {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Long getSalary() {
+            return salary;
+        }
+
+        @Override
+        public String toString() {
+            return "Employee{" +
+                    "name='" + name + '\'' +
+                    ", salary=" + salary +
+                    '}';
+        }
+    }
+
+    static class Department {
+        private final String name;
+        private final String code;
+        private final List<Employee> employees;
+
+        Department(String name, String code, List<Employee> employees) {
+            this.name = name;
+            this.code = code;
+            this.employees = employees;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public List<Employee> getEmployees() {
+            return employees;
+        }
+
+        @Override
+        public String toString() {
+            return "Department{" +
+                    "name='" + name + '\'' +
+                    ", code='" + code + '\'' +
+                    ", employees=" + employees +
+                    '}';
+        }
+    }
+}
