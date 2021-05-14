b894b0403687051e33610d14e2e2004b66347c08
denamyte <denamyte@gmail.com>
2021-05-14 04:20:12 +0300

    2021-05-14: 5 problems

--- Topics/Knuth-Morris-Pratt algorithm in Java/Finding a string in a matrix (v1)/src/Main.java
+++ Topics/Knuth-Morris-Pratt algorithm in Java/Finding a string in a matrix (v1)/src/Main.java
@@ -1,5 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        // put your code here...
+        Scanner scanner = new Scanner(System.in);
+        String pattern = scanner.nextLine();
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+        scanner.nextLine();
+
+        String text = Stream.generate(scanner::nextLine).limit(rows).collect(Collectors.joining(""));
+
+        final List<Integer> occurrences = kmpSearch(text, pattern);
+
+        System.out.println(occurrences.size());
+        occurrences.stream()
+                .map(i -> new int[]{i / cols, i % cols})
+                .forEach(ar -> System.out.printf("%d %d%n", ar[0], ar[1]));
     }
+
+    public static int[] prefixFunction(String str) {
+        int[] prefixFunc = new int[str.length()];
+
+        for (int i = 1; i < str.length(); i++) {
+            int j = prefixFunc[i - 1];
+
+            while (j > 0 && str.charAt(i) != str.charAt(j)) {
+                j = prefixFunc[j - 1];
+            }
+
+            if (str.charAt(i) == str.charAt(j)) {
+                j += 1;
+            }
+
+            prefixFunc[i] = j;
+        }
+
+        return prefixFunc;
+    }
+
+    public static List<Integer> kmpSearch(String text, String pattern) {
+        int[] prefixFunc = prefixFunction(pattern);
+        ArrayList<Integer> occurrences = new ArrayList<>();
+        int j = 0;
+        for (int i = 0; i < text.length(); i++) {
+            while (j > 0 && text.charAt(i) != pattern.charAt(j)) {
+                j = prefixFunc[j - 1];
+            }
+            if (text.charAt(i) == pattern.charAt(j)) {
+                j += 1;
+            }
+            if (j == pattern.length()) {
+                occurrences.add(i - j + 1);
+                j = prefixFunc[j - 1];
+            }
+        }
+        return occurrences;
+    }
+
 }
--- Topics/Rabin-Karp algorithm in Java/The longest repetitive substring/src/Main.java
+++ Topics/Rabin-Karp algorithm in Java/The longest repetitive substring/src/Main.java
@@ -1,5 +1,65 @@
+import java.util.*;
+
 public class Main {
+
+    static final int POW = 49157;
+    static final long MOD = 10_000_000_000_009L;
+
     public static void main(String[] args) {
-        // put your code here
+        String s = "";
+        try (Scanner scanner = new Scanner(System.in)) {
+            s = scanner.nextLine();
+        }
+        System.out.println(longestRepetitiveSubstringLength(s));
     }
+
+    private static int longestRepetitiveSubstringLength(String s) {
+        int lo = 1;
+        int hi = s.length() - 1;
+
+        // Binary search loop
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (hasRepetitiveSubstringOfLength(s, mid)) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return lo == 0 ? 0 : lo - 1;
+    }
+
+    private static boolean hasRepetitiveSubstringOfLength(String s, int k) {
+        Set<Long> hashes = new HashSet<>();
+
+        long hash = 0;
+        long pow = 1;
+
+        // The hash for the substring at the end of the string
+        for (int i = s.length() - k; i < s.length(); i++) {
+            if (i > s.length() - k) {
+                pow = pow * POW % MOD;
+            }
+            hash += s.charAt(i) * pow;
+            hash %= MOD;
+        }
+        hashes.add(hash);
+
+        for (int i = s.length() - 1 - k; i >= 0; i--) {
+            hash = rollHash(hash, pow, s.charAt(i + k), s.charAt(i));
+
+            if (hashes.contains(hash)) {
+                return true;
+            }
+            hashes.add(hash);
+        }
+
+        return false;
+    }
+
+    private static long rollHash(long hash, long pow, char removeChar, char nextChar) {
+        long hash2 = (hash - removeChar * pow % MOD + MOD) * POW % MOD;
+        return (hash2 + nextChar) % MOD;
+    }
+
 }
