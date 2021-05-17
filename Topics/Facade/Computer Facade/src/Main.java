5e70f2f26a376275dea948b48bae11ef73ee0292
denamyte <denamyte@gmail.com>
2021-05-18 02:54:22 +0300

    2021-05-18: 3 problems

---
+++ Topics/Facade/Computer Facade/src/Main.java
@@ -0,0 +1,80 @@
+class ComputerFacadeTestDrive {
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        Monitor monitor = new Monitor();
+        Keyboard keyboard = new Keyboard();
+
+        ComputerFacade computerFacade = new ComputerFacade(processor, monitor, keyboard);
+
+        computerFacade.turnOnComputer();
+        computerFacade.turnOffComputer();
+    }
+}
+
+class ComputerFacade {
+
+    private final Processor processor;
+    private final Monitor monitor;
+    private final Keyboard keyboard;
+
+    public ComputerFacade(Processor processor, Monitor monitor, Keyboard keyboard) {
+        this.processor = processor;
+        this.monitor = monitor;
+        this.keyboard = keyboard;
+    }
+
+    public void turnOnComputer() {
+        processor.on();
+        monitor.on();
+        keyboard.on();
+    }
+
+    public void turnOffComputer() {
+        keyboard.off();
+        monitor.off();
+        processor.off();
+    }
+}
+
+class Processor {
+
+    public void on() {
+        System.out.println("Processor on");
+    }
+
+    public void off() {
+        System.out.println("Processor off");
+    }
+}
+
+class Monitor {
+
+    public void on() {
+        System.out.println("Monitor on");
+    }
+
+    public void off() {
+        System.out.println("Monitor off");
+    }
+}
+
+class Keyboard {
+
+    public void on() {
+        System.out.println("Keyboard on");
+        turnOnBacklight();
+    }
+
+    public void off() {
+        System.out.println("Keyboard off");
+        turnOffBacklight();
+    }
+
+    private void turnOnBacklight() {
+        System.out.println("Backlight is turned on");
+    }
+
+    private void turnOffBacklight() {
+        System.out.println("Backlight is turned off");
+    }
+}
