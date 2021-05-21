dfe8fc87f6bc0140401cf3c96874081266b51353
denamyte <denamyte@gmail.com>
2021-05-21 03:05:16 +0300

    2021-05-21: 2 problems

--- Topics/Rabin-Karp algorithm in Java/The number of equal substring pairs/src/Main.java
+++ Topics/Rabin-Karp algorithm in Java/The number of equal substring pairs/src/Main.java
@@ -1,31 +1,46 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     static final int A = 53;
     static final long M = 1_000_000_009;
+    static long[] powerCache;
+    static long[] hashes;
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             final String s = scanner.nextLine();
             int count = scanner.nextInt();
 
-            final long[] hashes = calcPrefixHashes(s);
+            calcPrefixHashesAndPowers(s);
 
             int equalsCounter = 0;
             while (count-- > 0) {
-                long modMult = 1;
                 final int[][] ranges = sortRangeSet(inputRangeSet(scanner));
-                modMult = advanceMultiplier(modMult, ranges[0][0]);
-                final long hash1 = calcRangeHash(hashes, ranges[0], modMult);
-                modMult = advanceMultiplier(modMult, ranges[1][0] - ranges[0][0]);
-                final long hash2 = calcRangeHash(hashes, ranges[1], modMult);
+                final long hash1 = calcRangeHash(hashes, ranges[0], powerCache[ranges[1][0] - ranges[0][0]]);
+                final long hash2 = calcRangeHash(hashes, ranges[1], 1);
                 if (hash1 == hash2) {
                     equalsCounter++;
                 }
             }
 
             System.out.println(equalsCounter);
+        }
+    }
+
+    private static void calcPrefixHashesAndPowers(String s) {
+        hashes = new long[s.length()];
+        powerCache = new long[s.length()];
+        long hash = 0;
+        long pow = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            powerCache[i] = pow;
+            hash += s.charAt(i) * pow;
+            hash %= M;
+            hashes[i] = hash;
+
+            pow = pow * A % M;
         }
     }
 
@@ -43,37 +58,9 @@ public class Main {
         return new int[]{scanner.nextInt(), scanner.nextInt()};
     }
 
-    private static long[] calcPrefixHashes(String s) {
-        long[] hashes = new long[s.length()];
-        long hash = 0;
-        long pow = 1;
-
-        for (int i = 0; i < s.length(); i++) {
-//            hash += charToLong(s.charAt(i)) * pow;
-            hash += s.charAt(i) * pow;
-            hash %= M;
-            hashes[i] = hash;
-
-            pow = pow * A % M;
-        }
-        return hashes;
-    }
-
-    private static long calcRangeHash(long[] hashes, int[] range, long modMult) {
+    private static long calcRangeHash(long[] hashes, int[] range, long powDiff) {
         final long hash = (hashes[range[1] - 1] - (range[0] == 0 ? 0 : hashes[range[0] - 1]) + M) % M;
-        return hash * modMult % M;
-    }
-
-    private static long advanceMultiplier(long multiplier, int count) {
-        long m = multiplier;
-        for (int i = 0; i < count; i++) {
-            m = m * A % M;
-        }
-        return m;
-    }
-
-    private static long charToLong(char ch) {
-        return ch - 'A' + 1;
+        return hash * powDiff % M;
     }
 
 }
