d8bc48aea06367a20b1357f724743ee191830ad1
denamyte <denamyte@gmail.com>
2021-06-23 13:45:37 +0300

    2021-06-23: 1 problem

--- Topics/LocalDate/The first day of a month or not/src/Main.java
+++ Topics/LocalDate/The first day of a month or not/src/Main.java
@@ -1,5 +1,11 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        try (Scanner scanner = new Scanner(System.in)) {
+            LocalDate date = LocalDate.ofYearDay(scanner.nextInt(), scanner.nextInt());
+            System.out.println(date.getDayOfMonth() == 1);
+        }
     }
 }
