f34ff87396d5f8a534d4f7b6ac28e46fa816c7cf
denamyte <denamyte@gmail.com>
2021-06-29 13:34:42 +0300

    2021-06-29: 1 problem

--- Topics/LocalTime/Some hours and minutes ago/src/Main.java
+++ Topics/LocalTime/Some hours and minutes ago/src/Main.java
@@ -1,5 +1,13 @@
+import java.time.LocalTime;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        try (Scanner scanner = new Scanner(System.in)) {
+            LocalTime time = LocalTime.parse(scanner.nextLine())
+                    .minusHours(scanner.nextInt())
+                    .minusMinutes(scanner.nextInt());
+            System.out.println(time);
+        }
     }
 }
