9f7a6de9616352aa2886a644df2a7f33afbdc85c
denamyte <denamyte@gmail.com>
2021-07-15 15:26:11 +0300

    2021-07-15: 1 problem

--- Topics/Choosing the right collection/Even numbers go first/src/Main.java
+++ Topics/Choosing the right collection/Even numbers go first/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.function.BiConsumer;
+import java.util.stream.IntStream;
+
 class Main {
+    private static final BiConsumer<Deque<Integer>, Integer> ACCUMULATOR = (list, i) -> {
+        if ((i & 1) == 1) {
+            list.addLast(i);
+        } else {
+            list.addFirst(i);
+        }
+    };
+
     public static void main(String[] args) {
-        // put your code here
+        try (Scanner scanner = new Scanner(System.in)) {
+            IntStream.generate(scanner::nextInt).limit(scanner.nextInt())
+                    .boxed().collect(LinkedList::new, ACCUMULATOR, (l1, l2) -> { })
+                    .forEach(System.out::println);
+
+        }
     }
 }
