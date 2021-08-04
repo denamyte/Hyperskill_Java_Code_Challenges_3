fbc1a045cc75d12a5c9a468fd5bdfc68550b66b5
denamyte <denamyte@gmail.com>
2021-08-04 22:10:42 +0300

    Project updated

--- Topics/Manipulating fields and methods/Invoking a method/src/Main.java
+++
@@ -1,7 +0,0 @@
-import java.lang.reflect.InvocationTargetException;
-
-class Main {
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
-        System.out.println(SomeClass.class.getDeclaredMethods()[0].invoke(new SomeClass()));
-    }
-}
