eadac17a28c74e1dc0dfe88ca149c01c9c845ea2
denamyte <denamyte@gmail.com>
2021-08-15 15:27:59 +0300

    2021-08-15: 1 problem

--- Topics/LocalDate/The n-th day from the end of a month/src/Main.java
+++ Topics/LocalDate/The n-th day from the end of a month/src/Main.java
@@ -1,5 +1,11 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println(LocalDate.of(scanner.nextInt(), scanner.nextInt(), 1)
+                                       .plusMonths(1).minusDays(scanner.nextInt()));
+        }
     }
 }
