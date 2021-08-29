de5e7f95d55640c298eb3d9a7255e12294754470
denamyte <denamyte@gmail.com>
2021-08-29 15:30:31 +0300

    2021-08-29: 1 problem

--- Topics/LocalDateTime/The passed hours since the beginning of year/src/Main.java
+++ Topics/LocalDateTime/The passed hours since the beginning of year/src/Main.java
@@ -1,5 +1,12 @@
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        final LocalDateTime dateTime = LocalDateTime.parse(new Scanner(System.in).nextLine());
+        final long amount = LocalDateTime.of(dateTime.getYear(), 1, 1, 0, 0)
+                .until(dateTime, ChronoUnit.HOURS);
+        System.out.println(amount);
     }
 }
