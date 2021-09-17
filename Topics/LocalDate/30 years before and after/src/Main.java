cfe72458b3ccf519f762db8dce2ba8200b0320bf
denamyte <denamyte@gmail.com>
2021-09-18 01:21:57 +0300

    2021-09-17: 1 problem

--- Topics/LocalDate/30 years before and after/src/Main.java
+++ Topics/LocalDate/30 years before and after/src/Main.java
@@ -1,5 +1,13 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        try (Scanner scanner = new Scanner(System.in)) {
+            final LocalDate date = LocalDate.parse(scanner.nextLine());
+            IntStream.of(-30, 30).mapToObj(i -> date.withYear(date.getYear() + i))
+                    .forEach(System.out::println);
+        }
     }
 }
